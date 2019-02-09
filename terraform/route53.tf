e491c983a060e856f734aa440f0e4f8085e2555d
Don O'Neill <sntxrr+github@gmail.com>
2019-02-08 21:08:30 -0800

    correct lb to alb (#16)
    * correct lb to alb
    
    * fix zone name
    
    * Update main.workflow
    
    * properly wiring th is in
    
    * Update main.workflow

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -4,13 +4,13 @@ resource "aws_route53_zone" "slackin" {
 }
 
 resource "aws_route53_record" "alias_route53_record" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
+  zone_id = "${aws_route53_zone.slackin.zone_id}"
   name    = "invite.sntxrr.com"
   type    = "A"
 
   alias {
-    name                   = "${aws_lb.slackin.dns_name}"
-    zone_id                = "${aws_lb.slackin.zone_id}"
+    name                   = "${aws_alb.slackin.dns_name}"
+    zone_id                = "${aws_alb.slackin.zone_id}"
     evaluate_target_health = true
   }
 }
